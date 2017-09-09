d709f8717d2b724c7c717524fe24745e1880d660
Diego Jenzer <diego@mbp-diego.home>
2017-09-09 10:19:51 +0100

    new ML course started

--- ML-finance/intraQuarter_Analysis.py
+++ ML-finance/intraQuarter_Analysis.py
@@ -8,19 +8,34 @@ path = "./intraQuarter"
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path+"/_KeyStats/"
     stock_list = [x[0] for x in os.walk(statspath)]
-    #print(stock_list)
+    df = pd.DataFrame(columns = ["Date", "Unix", "Ticker", "DE Ratio"])
+
 
     for each_dir in stock_list[1:]:
         each_file = os.listdir(each_dir)
         #print(each_file)
         #time.sleep(15)
-
+        ticker = each_dir.split("_KeyStats/")[1]
         if len(each_file) > 0:
             for file in each_file:
                 date_stamp = datetime.strptime(file, "%Y%m%d%H%M%S.html")
                 unix_time = time.mktime(date_stamp.timetuple())
-                print(date_stamp, unix_time)
-                time.sleep(15)
+                #print(date_stamp, unix_time)
+                full_file_path = each_dir+"/"+file
+                #print(full_file_path)
+                source = open(full_file_path, "r").read()
+                #print(source)
+                try:
+                    value = float(source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split("</td>")[0])
+                    df = df.append({"Date":date_stamp, "Unix":unix_time, "Ticker":ticker, "DE Ratio":value}, ignore_index=True)
+                except Exception as e:
+                    pass
+
+                #print(ticker+":", value)
+    save = gather.replace(" ","").replace(")","").replace("(","").replace("/","")+(".csv")
+    print(save)
+    df.to_csv(save)
+
 
 Key_Stats()
 
