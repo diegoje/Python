f15ca9bbee1f1c79967822db33ac5e8464931adf
diegoje <diegojenzer@hotmail.com>
2017-09-17 12:46:20 +0100

    new intro msg

--- test.py
+++ test.py
@@ -4,4 +4,25 @@ print("dP                dP                 .d888888   888888ba\n"+
       "88'  `88 88'  `88 88'  `88 88'  `88 88     88   88\n"+
       "88.  .88 88.  .88 88.  .88 88.  .88 88     88   88\n"+
       "88Y8888' `88888P8 88Y8888' `88888P8 88     88   dP\n"+
-      "                                         by Diego Jenzer\n")
+      "                                         by Diego Jenzer\n\n")
+
+print("[?] What do you want to do?\n"+
+      "    1. Start new AP.\n"+
+      "    2. Stop AP.\n"+
+      "    3. See stats.\n")
+
+while True:
+      first_question = input("Your selection: ")
+
+      if first_question == "1" or first_question == "2" or first_question == "3":
+            break
+      else:
+            print("[!] Please select an option.")
+
+
+if first_question == "1":
+      print("[1]")
+elif first_question == "2":
+      print("[2]")
+else:
+      print("[3]")
